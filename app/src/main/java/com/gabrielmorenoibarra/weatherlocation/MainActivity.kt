004084591dc9bafd78d32820647f98d241445bd8
GabrielMorenoIbarra <gabrielmorenoibarra@gmail.com>
2019-08-04 08:34:32 +0200

    Google Maps working

--- app/src/main/java/com/gabrielmorenoibarra/weatherlocation/MainActivity.kt
+++ app/src/main/java/com/gabrielmorenoibarra/weatherlocation/MainActivity.kt
@@ -2,11 +2,37 @@ package com.gabrielmorenoibarra.weatherlocation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var googleMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initGoogleMap()
+    }
+
+    private fun initGoogleMap() {
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.googleMap) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap?) {
+        googleMap?.let {
+            this.googleMap = it
+
+            // Add a marker in Sydney and move the camera
+            val sydney = LatLng(-34.0, 151.0)
+            this.googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+            this.googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        }
     }
 }
